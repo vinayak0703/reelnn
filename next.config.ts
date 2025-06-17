8fad2c6d9c64c0cb0bd6cb5febd36df7bdff9dd3
vinayak0703 <72682722+vinayak0703@users.noreply.github.com>
2025-06-17 18:25:09 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
-    unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
